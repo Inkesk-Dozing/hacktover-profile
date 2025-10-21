629b8f68ebb8a50e5fafe03f7a9e734c3d5606dd
Manas Bhasker <bhaskermanas73@gmail.com>
2025-10-21 16:19:23 +0530

    simple multiplication program

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+# Simple multiplication program
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+result = a * b
+print("The result of multiplication is:", result)
