12d5d26be441858621383f3f03a92c2530ecc432
Manas Bhasker <bhaskermanas73@gmail.com>
2025-10-21 16:14:59 +0530

    Add simple division program

---
+++ division.py
@@ -0,0 +1,9 @@
+# Simple division program
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if b != 0:
+    result = a / b
+    print("The result of division is:", result)
+else:
+    print("Error: Division by zero is not allowed.")
